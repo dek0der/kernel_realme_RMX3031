895f8e4a7c89278a31cbdf11b77e08bacab2ac0a
dek0der <umar.hac@gmail.com>
2024-05-08 13:40:21 +0000

    compile.sh: update -j flag of make command to use all available cores

--- compile.sh
+++ compile.sh
@@ -7,4 +7,4 @@ make O=out ARCH=arm64 CC=clang CROSS_COMPILE=aarch64-linux-gnu- CONFIG_NO_ERROR_
 mkdir -p $WDIR/out/hal/phydm/
 cp drivers/staging/rtl8192eu/hal/phydm/phydm.mk $WDIR/out/hal/phydm/
 
-make -j16 O=out ARCH=arm64 CC=clang CROSS_COMPILE=aarch64-linux-gnu- CONFIG_NO_ERROR_ON_MISMATCH=y CROSS_COMPILE_ARM32=arm-linux-gnueabi- LLVM=1 LLVM_IAS=1
+make -j$(nproc --all) O=out ARCH=arm64 CC=clang CROSS_COMPILE=aarch64-linux-gnu- CONFIG_NO_ERROR_ON_MISMATCH=y CROSS_COMPILE_ARM32=arm-linux-gnueabi- LLVM=1 LLVM_IAS=1
